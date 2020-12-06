52c6180417fd1412c7c1ed9a35350aee2001e9c5
MachoPiggies <danieljdean86@gmail.com>
2020-12-06 00:00:06 +0000

    Panicking event is now cancellable

--- src/main/java/com/machopiggies/famedpanicapi/events/PanickingUpdateEvent.java
+++ src/main/java/com/machopiggies/famedpanicapi/events/PanickingUpdateEvent.java
@@ -2,10 +2,11 @@ package com.machopiggies.famedpanicapi.events;
 
 import com.machopiggies.famedpanicapi.misc.PanicData;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class PanickingUpdateEvent extends Event {
+public class PanickingUpdateEvent extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
     private final boolean add;
@@ -13,11 +14,14 @@ public class PanickingUpdateEvent extends Event {
     private final Player player;
     private final Player remover;
 
+    private boolean cancelled;
+
     public PanickingUpdateEvent(boolean add, PanicData data) {
         this.add = add;
         this.data = data;
         this.player = data.player;
         this.remover = null;
+        cancelled = false;
     }
 
     public PanickingUpdateEvent(boolean add, Player player) {
@@ -25,6 +29,7 @@ public class PanickingUpdateEvent extends Event {
         this.data = null;
         this.player = player;
         this.remover = null;
+        cancelled = false;
     }
 
     public PanickingUpdateEvent(boolean add, PanicData data, Player remover) {
@@ -32,6 +37,7 @@ public class PanickingUpdateEvent extends Event {
         this.data = data;
         this.player = data.player;
         this.remover = remover;
+        cancelled = false;
     }
 
     public PanickingUpdateEvent(boolean add, Player player, Player remover) {
@@ -39,6 +45,7 @@ public class PanickingUpdateEvent extends Event {
         this.data = null;
         this.player = player;
         this.remover = remover;
+        cancelled = false;
     }
 
     public boolean isAdd() {
@@ -64,5 +71,15 @@ public class PanickingUpdateEvent extends Event {
     @Override
     public HandlerList getHandlers() {
         return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 }
