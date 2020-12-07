be457e09e6057cd91a7800b756869cf8da1c2e19
MachoPiggies <danieljdean86@gmail.com>
2020-12-07 03:27:11 +0000

    Removing enum privatisation

--- src/main/java/com/machopiggies/famedpanicapi/misc/Request.java
+++ src/main/java/com/machopiggies/famedpanicapi/misc/Request.java
@@ -16,7 +16,7 @@ public class Request extends Event {
 
     private static final HandlerList handlers = new HandlerList();
 
-    private enum A {
+    public enum A {
         a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
     }
 
