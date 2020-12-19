3bf6a3ca797309fe99ea730999cd585ebe2cd42e
MachoPiggies <danieljdean86@gmail.com>
2020-12-19 03:35:16 +0000

    Removed canChangeSettings setting

--- src/main/java/com/machopiggies/famedpanicapi/misc/APISettings.java
+++ src/main/java/com/machopiggies/famedpanicapi/misc/APISettings.java
@@ -6,14 +6,12 @@ public class APISettings {
     public boolean canChangeSafemode;
     public boolean canChangePanicking;
     public boolean canChangeInspector;
-    public boolean canChangeSettings;
 
-    public APISettings(boolean enabled, boolean canAccessTokens, boolean canChangeSafemode, boolean canChangePanicking, boolean canChangeInspector, boolean canChangeSettings) {
+    public APISettings(boolean enabled, boolean canAccessTokens, boolean canChangeSafemode, boolean canChangePanicking, boolean canChangeInspector) {
         this.enabled = enabled;
         this.canAccessTokens = canAccessTokens;
         this.canChangeSafemode = canChangeSafemode;
         this.canChangePanicking = canChangePanicking;
         this.canChangeInspector = canChangeInspector;
-        this.canChangeSettings = canChangeSettings;
     }
 }
